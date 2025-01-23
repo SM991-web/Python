5b577a0714b12f233e6e021e05c735abea28ca75
SM991-web <mukhopadhyaysnehanshu@gmail.com>
2025-01-23 18:56:12 +0530

    commit

--- Operators.py
+++ Operators.py
@@ -1,29 +1,29 @@
-# # operators
-#
-# x=["apple","banana"]
-# y=["apple","banana"]
-# z=x
-# # identity operator
-#
-# print(x is y) #loaction and address is not same
-# print(x is z) #both have same location
-# print(x == y)
-# print(x is not y) # return True
-#
-# #memebership operators
-#
-# print("apple" in x)
-# print("apple" in y)
-# print("banana" in z)
-#
-# # bitwise operator
-#
-# print(1&3)
-# print(2|5)
-# print(2^5)
-# print(~5)
-# print(2<<5)
-# print(34>>4)
+# operators
+
+x=["apple","banana"]
+y=["apple","banana"]
+z=x
+# identity operator
+
+print(x is y) #loaction and address is not same
+print(x is z) #both have same location
+print(x == y)
+print(x is not y) # return True
+
+#memebership operators
+
+print("apple" in x)
+print("apple" in y)
+print("banana" in z)
+
+# bitwise operator
+
+print(1&3)
+print(2|5)
+print(2^5)
+print(~5)
+print(2<<5)
+print(34>>4)
 
 #logical operator
 print(7 and 2 or 8 or 5 and 8)
